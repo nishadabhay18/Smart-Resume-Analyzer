a1095f989981bfb9557d6e058869ac96a34291e5
nishadabhay18 <abhayn927@gmail.com>
2026-09-17 17:20:14 +0530

    commit

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -71,7 +71,8 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    // baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_SERVER_URL,
     withCredentials: true
 })
 
